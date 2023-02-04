503e5f5e6180859dfb1db42087fd2b2d29ccb7c6
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-04 19:58:18 +0600

    Update o.py

--- o.py
+++ o.py
@@ -1,1 +1,13 @@
-import num
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import num
+
+else:
+	os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+	time.sleep(2)
+    import num1
